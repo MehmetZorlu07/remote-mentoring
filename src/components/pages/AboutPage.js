737a1ad57eb73d19fe5bbc8fb7c4becbc536dc89
Mehmet Zorlu <2328759z@student.gla.ac.uk>
2020-10-16 08:26:00 +0300

    Closes #39 Need more data to continue about us page.

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 function AboutPage() {
-  return <p>AboutPage works!</p>;
+  return (
+    <Jumbotron>
+      <h1>About Us</h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar
+        ex non ornare cursus. Vestibulum vitae justo tortor. Fusce ut ipsum a
+        metus tempus feugiat ac sed arcu. Phasellus vitae libero et leo lacinia
+        eleifend. Integer molestie turpis risus, non lacinia nibh dictum et.
+        Donec augue sem, dictum vitae lobortis lobortis, viverra quis magna.
+        Curabitur nec nunc pretium, congue mi fermentum, ullamcorper nibh.
+        Maecenas facilisis odio at ligula dapibus, ac ultricies lacus luctus.
+        Phasellus faucibus leo aliquet tristique volutpat. Sed a pretium velit.
+        Mauris a ligula porttitor, sodales justo in, mattis sapien. Praesent
+        ornare porttitor libero, eget congue metus. Suspendisse dignissim nunc
+        eget metus vestibulum, non finibus turpis placerat. Morbi lacus velit,
+        fringilla vel fermentum in, laoreet at odio. Pellentesque consectetur
+        sodales tellus nec lacinia. Nullam et purus dapibus neque vulputate
+        faucibus. Sed et ligula ut nunc lacinia lacinia sed sed magna.
+        Pellentesque est lacus, molestie non velit at, aliquam euismod risus.
+        Proin augue nisi, convallis et fermentum et, luctus eu leo. Ut in orci
+        ante. Etiam nec congue odio. Donec sollicitudin neque in mauris pretium
+        tristique. Phasellus arcu libero, aliquam sit amet quam sit amet,
+        vestibulum sollicitudin lorem. Mauris vitae nulla dignissim, feugiat sem
+        in, tincidunt arcu. Vivamus ipsum arcu, euismod et velit sed, tincidunt
+        eleifend tortor. Vestibulum elit leo, eleifend sed odio ut, pretium
+        placerat odio.{" "}
+      </p>
+    </Jumbotron>
+  );
 }
 
 export default AboutPage;
