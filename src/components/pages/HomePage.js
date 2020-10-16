38dfafe46cc2f0b8b6de0910209f8f475bc712f8
Mehmet Zorlu <2328759z@student.gla.ac.uk>
2020-10-16 08:10:23 +0300

    Closes #38 The basic is ready, needs design improvements.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,19 +1,16 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
 import Hero from "../Hero/Hero";
-import Jumbotron from "react-bootstrap/Jumbotron"
-import Button from "react-bootstrap/Button"
-
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
 
 function HomePage(props) {
   return (
     <div>
       <Hero />
       <Jumbotron>
-        <h1>Categories</h1>
-         <p>
-          Checkout these project categories below that might interest you!
-        </p>
+        <h1>Research Categories</h1>
+        <p>Checkout these project categories below that might interest you!</p>
         <p>
           <Button variant="primary">Learn more</Button>
         </p>
