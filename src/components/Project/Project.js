8c4b873776c4d7e4b3c8053741c37d493d09ae29
Mehmet Zorlu <2328759z@student.gla.ac.uk>
2020-10-27 16:04:21 +0200

    Closes #54 Projects are shown in projects page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,137 +1,19 @@
 import React from "react";
 import "./Project.css";
 
-function Project() {
-  return (
-    <div className="container">
-      <div className="card">
-        <h1>Project A</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          suscipit facilisis lorem, vitae lobortis sem congue quis. Suspendisse
-          velit enim, pulvinar ut vehicula sed, dictum quis mi. Proin
-          scelerisque sodales feugiat. Cras nisi magna, commodo ut ligula eget,
-          lacinia dignissim justo. Maecenas ornare scelerisque justo, sit amet
-          vestibulum ipsum consequat nec. Phasellus condimentum, erat eget
-          sodales bibendum, mi elit sollicitudin ligula, vel dapibus urna nisl
-          non lacus. Proin quis orci dictum, aliquam dolor ac, aliquam metus.
-          Phasellus porta enim massa, et convallis felis viverra ut. Donec sed
-          luctus quam. Donec ut interdum dui. Etiam varius nibh in auctor
-          interdum. Vivamus nec turpis nec nibh pharetra bibendum. Mauris
-          tincidunt sit amet enim vitae mollis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Etiam viverra
-          nibh eget dolor tempus, non commodo ante porttitor.{" "}
-        </p>
-        <p>Tags: Mathematics, Binomial</p>
-        <p>Academic: Wolverine</p>
+class Project extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="card">
+          <h2>{this.props.name}</h2>
+          <p>{this.props.description}</p>
+          <p>{this.props.academicID}</p>
+          <p>Tags: Mathematics, Binomial</p>
+        </div>
       </div>
-      <div className="card">
-        <h1>Project A</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          suscipit facilisis lorem, vitae lobortis sem congue quis. Suspendisse
-          velit enim, pulvinar ut vehicula sed, dictum quis mi. Proin
-          scelerisque sodales feugiat. Cras nisi magna, commodo ut ligula eget,
-          lacinia dignissim justo. Maecenas ornare scelerisque justo, sit amet
-          vestibulum ipsum consequat nec. Phasellus condimentum, erat eget
-          sodales bibendum, mi elit sollicitudin ligula, vel dapibus urna nisl
-          non lacus. Proin quis orci dictum, aliquam dolor ac, aliquam metus.
-          Phasellus porta enim massa, et convallis felis viverra ut. Donec sed
-          luctus quam. Donec ut interdum dui. Etiam varius nibh in auctor
-          interdum. Vivamus nec turpis nec nibh pharetra bibendum. Mauris
-          tincidunt sit amet enim vitae mollis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Etiam viverra
-          nibh eget dolor tempus, non commodo ante porttitor.{" "}
-        </p>
-        <p>Tags: Mathematics, Binomial</p>
-        <p>Academic: Wolverine</p>
-      </div>
-      <div className="card">
-        <h1>Project A</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          suscipit facilisis lorem, vitae lobortis sem congue quis. Suspendisse
-          velit enim, pulvinar ut vehicula sed, dictum quis mi. Proin
-          scelerisque sodales feugiat. Cras nisi magna, commodo ut ligula eget,
-          lacinia dignissim justo. Maecenas ornare scelerisque justo, sit amet
-          vestibulum ipsum consequat nec. Phasellus condimentum, erat eget
-          sodales bibendum, mi elit sollicitudin ligula, vel dapibus urna nisl
-          non lacus. Proin quis orci dictum, aliquam dolor ac, aliquam metus.
-          Phasellus porta enim massa, et convallis felis viverra ut. Donec sed
-          luctus quam. Donec ut interdum dui. Etiam varius nibh in auctor
-          interdum. Vivamus nec turpis nec nibh pharetra bibendum. Mauris
-          tincidunt sit amet enim vitae mollis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Etiam viverra
-          nibh eget dolor tempus, non commodo ante porttitor.{" "}
-        </p>
-        <p>Tags: Mathematics, Binomial</p>
-        <p>Academic: Wolverine</p>
-      </div>
-      <div className="card">
-        <h1>Project A</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          suscipit facilisis lorem, vitae lobortis sem congue quis. Suspendisse
-          velit enim, pulvinar ut vehicula sed, dictum quis mi. Proin
-          scelerisque sodales feugiat. Cras nisi magna, commodo ut ligula eget,
-          lacinia dignissim justo. Maecenas ornare scelerisque justo, sit amet
-          vestibulum ipsum consequat nec. Phasellus condimentum, erat eget
-          sodales bibendum, mi elit sollicitudin ligula, vel dapibus urna nisl
-          non lacus. Proin quis orci dictum, aliquam dolor ac, aliquam metus.
-          Phasellus porta enim massa, et convallis felis viverra ut. Donec sed
-          luctus quam. Donec ut interdum dui. Etiam varius nibh in auctor
-          interdum. Vivamus nec turpis nec nibh pharetra bibendum. Mauris
-          tincidunt sit amet enim vitae mollis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Etiam viverra
-          nibh eget dolor tempus, non commodo ante porttitor.{" "}
-        </p>
-        <p>Tags: Mathematics, Binomial</p>
-        <p>Academic: Wolverine</p>
-      </div>
-      <div className="card">
-        <h1>Project A</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          suscipit facilisis lorem, vitae lobortis sem congue quis. Suspendisse
-          velit enim, pulvinar ut vehicula sed, dictum quis mi. Proin
-          scelerisque sodales feugiat. Cras nisi magna, commodo ut ligula eget,
-          lacinia dignissim justo. Maecenas ornare scelerisque justo, sit amet
-          vestibulum ipsum consequat nec. Phasellus condimentum, erat eget
-          sodales bibendum, mi elit sollicitudin ligula, vel dapibus urna nisl
-          non lacus. Proin quis orci dictum, aliquam dolor ac, aliquam metus.
-          Phasellus porta enim massa, et convallis felis viverra ut. Donec sed
-          luctus quam. Donec ut interdum dui. Etiam varius nibh in auctor
-          interdum. Vivamus nec turpis nec nibh pharetra bibendum. Mauris
-          tincidunt sit amet enim vitae mollis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Etiam viverra
-          nibh eget dolor tempus, non commodo ante porttitor.{" "}
-        </p>
-        <p>Tags: Mathematics, Binomial</p>
-        <p>Academic: Wolverine</p>
-      </div>
-      <div className="card">
-        <h1>Project A</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-          suscipit facilisis lorem, vitae lobortis sem congue quis. Suspendisse
-          velit enim, pulvinar ut vehicula sed, dictum quis mi. Proin
-          scelerisque sodales feugiat. Cras nisi magna, commodo ut ligula eget,
-          lacinia dignissim justo. Maecenas ornare scelerisque justo, sit amet
-          vestibulum ipsum consequat nec. Phasellus condimentum, erat eget
-          sodales bibendum, mi elit sollicitudin ligula, vel dapibus urna nisl
-          non lacus. Proin quis orci dictum, aliquam dolor ac, aliquam metus.
-          Phasellus porta enim massa, et convallis felis viverra ut. Donec sed
-          luctus quam. Donec ut interdum dui. Etiam varius nibh in auctor
-          interdum. Vivamus nec turpis nec nibh pharetra bibendum. Mauris
-          tincidunt sit amet enim vitae mollis. Orci varius natoque penatibus et
-          magnis dis parturient montes, nascetur ridiculus mus. Etiam viverra
-          nibh eget dolor tempus, non commodo ante porttitor.{" "}
-        </p>
-        <p>Tags: Mathematics, Binomial</p>
-        <p>Academic: Wolverine</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Project;
--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -1,8 +1,43 @@
 import React from "react";
+import "./ProjectsPage.css";
 import Project from "../Project/Project";
 
-function ProjectsPage() {
-  return <Project />;
+class ProjectsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+    };
+    this.getProjects = this.getProjects.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+
+  getProjects = () => {
+    fetch("http://localhost:3000/projects")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ projects: data });
+      });
+  };
+
+  render() {
+    const projectsList = [];
+    for (const project of this.state.projects) {
+      projectsList.push(
+        <Project
+          key={project.id}
+          name={project.name}
+          description={project.description}
+          academicID={project.academicID}
+        />
+      );
+    }
+
+    return <div className="container">{projectsList}</div>;
+  }
 }
 
 export default ProjectsPage;
